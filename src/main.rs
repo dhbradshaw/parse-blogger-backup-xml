47cc174ee62218f0a93fb294e6bc9aeab4920734
d. h. bradshaw <dhbradshaw@gmail.com>
2022-02-18 08:04:10 -0600

    print posts and basic stats

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use parse_blogger_backup_xml::get_posts;
+
+/// Parse the backup.xml file from a Google Blogger backup.
+///
 fn main() {
-    dbg!("Hello, world!");
+    // Read backup file location from command line
+    let args: Vec<String> = std::env::args().collect();
+    let backup_file_path = &args[1];
+    let posts = get_posts(backup_file_path).unwrap();
+
+    // Print out posts
+    println!("{:#?}", &posts);
+
+    // Print out the posts length
+    let post_count = posts.len();
+    println!("\n{post_count} posts in total");
+
+    if !posts.is_empty() {
+        let first_post_published = &posts[0].published;
+        let last_post_published = &posts[post_count - 1].published;
+        println!("published from {first_post_published} to {last_post_published}");
+    }
 }
